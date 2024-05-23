413399849b0fd2b03d2ea8d1eda57942820a756d
EffahAdode3 <whatyousow00@gmail.com>
2024-05-23 04:46:24 -0700

    add the content

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login.vue'
 import Signpup from '../views/Signup.vue'
 import component from '../components/SideBarAndNav.vue'
+import Dashboard from  '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +22,11 @@ const router = createRouter({
       name: ' component ',
       component:  component 
     },
+    {
+      path: '/Dashboard',
+      name: ' Dashboard ',
+      component:  Dashboard 
+    },
   ]
 })
-
 export default router
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <component :is="SideBarAndNavVue"></component>
+       <component :is="Content"></component>
+    
+
+    </div>
+</template>
+<script>
+
+import Content from '../components/Content.vue'
+import SideBarAndNavVue from '../components/SideBarAndNav.vue'
+export default{
+    components:{
+    
+        Content,
+        SideBarAndNavVue,
+    },
+    data(){
+        return{
+           Content,
+           SideBarAndNavVue
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
