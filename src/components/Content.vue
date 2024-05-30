1d1038ecc4d2a83569839993fb3d29bb6e360668
EffahAdode3 <whatyousow00@gmail.com>
2024-05-30 07:40:07 -0700

    new on Add

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -13,10 +13,10 @@
     <div class="row m-1 p-3">
         <div class="col col-11 mx-auto">
             <div class="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
-                <!-- <div class="form-floating"> -->
+                <div class="form-floating">
   <textarea class="form-control" placeholder="Add new .." id="floatingTextarea2" v-model="newTodo"  style="height: 100px"></textarea>
   <!-- <label for="floatingTextarea2">Comments</label> -->
-<!-- </div> -->
+</div>
                 <!-- <div class="col">
                     <input class="form-control form-control-lg border-0 add-todo-input bg-transparent rounded" type="text" v-model="newTodo"  placeholder="Add new ..">
                 </div> -->
