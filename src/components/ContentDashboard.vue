c433a20ad15340cbf7698522563b1d5dfe664872
EffahAdode3 <whatyousow00@gmail.com>
2024-06-18 10:42:38 -0700

    Go commit Due explain

--- src/components/ContentDashboard.vue
+++ src/components/ContentDashboard.vue
@@ -3,7 +3,7 @@
     <br>
     <br>
     <br>
-    <table class="table">
+    <!-- <table class="table">
       <thead>
         <tr>
           <th scope="col">Date</th>
@@ -40,11 +40,6 @@
        </td>
 
 
-
-          <!-- <td><span @click="showFullText(toTolist.newTodo)">{{ truncateText(toTolist.newTodo) }}</span></td> -->
-          <!-- <td><a href="#" @click.prevent="showFullText(toTolist.newTodo)">{{ truncateText(toTolist.newTodo) }}</a></td> -->
-          <!-- <td>{{ new Date(toTolist.deadline).toDateString() }}</td> -->
-
               <td :class="{'overdue': isOverdue(toTolist.deadline)}">
             {{ new Date(toTolist.deadline).toDateString() }}
             <span v-if="isOverdue(toTolist.deadline)"> - Overdue!</span>
@@ -63,7 +58,64 @@
           </td>
         </tr>
       </tbody>
-    </table> 
+    </table>  -->
+
+
+    
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Categories
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#" @click="fetchAllData()">All Category</a></li>
+                <li><a class="dropdown-item" href="#" @click="fetchData('Personal')">Personal</a></li>
+                <li><a class="dropdown-item" href="#" @click="fetchData('I.T Service')">I.T Service</a></li>
+                <li><a class="dropdown-item" href="#" @click="fetchData('Networking')">Networking</a></li>
+                <li><a class="dropdown-item" href="#" @click="fetchData('End User')">End User</a></li>
+                <li><a class="dropdown-item" href="#" @click="fetchData('Cyber-Security')">Cyber-Security</a></li>
+              </ul>
+            </div>
+          </th>
+          <th scope="col">To Do Task</th>
+          <th scope="col">Due Date</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="toTolist in filteredTOListDos" :key="toTolist.id">
+          <td>{{ new Date(toTolist.createdAt).toDateString() }}</td>
+          <td>{{ toTolist.category }}</td>
+          <td>
+            <span @click="showFullText(toTolist.newTodo)"
+                  :title="toTolist.newTodo"
+                  class="truncate-text">
+              {{truncateText(toTolist.newTodo)}}
+            </span>
+          </td>
+          <td :class="{'overdue': isOverdue(toTolist.deadline)}">
+            {{ new Date(toTolist.deadline).toDateString() }}
+            <span v-if="isOverdue(toTolist.deadline)"> - Overdue!</span>
+          </td>
+          <td>
+            <div class="dropdown">
+              <button :class="['btn dropdown-toggle', statusButtonClass(toTolist.statuses)]" type="button" id="statusDropdownButton" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ toTolist.statuses }}
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="statusDropdownButton">
+                <li><a class="dropdown-item" href="#" @click="updateStatus(toTolist.id, 'Pending')">Pending</a></li>
+                <li><a class="dropdown-item" href="#" @click="updateStatus(toTolist.id, 'Completed')">Completed</a></li>
+                <li><a class="dropdown-item" href="#" @click="updateStatus(toTolist.id, 'In-Progress')">In-Progress</a></li>
+              </ul>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
 
 
@@ -120,6 +172,16 @@
            });
             
           },
+
+          computed: {
+    filteredTOListDos() {
+      const currentDate = new Date();
+      return this.TOListDos.filter(todo => {
+        const dueDate = new Date(todo.deadline);
+        return !(todo.statuses === 'Completed' && currentDate > dueDate);
+      });
+    }
+  },
 
        methods: {
         // fetch Data using the Category
