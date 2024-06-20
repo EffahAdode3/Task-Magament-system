c42265d0a56f2478f137134a1268f2b1154cb226
EffahAdode3 <whatyousow00@gmail.com>
2024-06-20 09:17:37 -0700

    new push to  content

--- src/components/ContentDashboard.vue
+++ src/components/ContentDashboard.vue
@@ -100,12 +100,13 @@
               {{truncateText(toTolist.newTodo)}}
             </span>
           </td>
+          <td>{{ new Date(toTolist.deadline).toDateString() }}</td>
 
-
-          <td :class="{'overdue': isOverdue(toTolist.deadline)}">
+          <!-- <td :class="{'overdue': isOverdue(toTolist.deadline)}">
             {{ new Date(toTolist.deadline).toDateString() }}
             <span v-if="isOverdue(toTolist.deadline)"> - Overdue!</span>
-          </td>
+          </td> -->
+
           <td>
 
 
@@ -326,9 +327,9 @@
     },
     
 
-    isCompletedAndOverdue(toTolist) {
-      return toTolist.statuses === 'Completed' && this.isOverdue(toTolist.deadline);
-    },
+    // isCompletedAndOverdue(toTolist) {
+    //   return toTolist.statuses === 'Completed' && this.isOverdue(toTolist.deadline);
+    // },
 
     // over  Date due
 
