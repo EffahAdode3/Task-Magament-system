c4d850c56a7db92855689c1d0711d8db1828b89d
EffahAdode3 <whatyousow00@gmail.com>
2024-06-23 18:41:10 +0000

    new An

--- src/components/ContentDashboard.vue
+++ src/components/ContentDashboard.vue
@@ -239,7 +239,7 @@
       <div class="modal-body">
         <input type="text" class="form-control" v-model="searchEmail" @input="searchUsers" placeholder="Search by email">
         <ul class="list-group mt-2">
-          <li class="list-group-item" v-for="user in searchedUsers" :key="user.id" @click="selectUser(user.id)">
+          <li class="list-group-item" v-for="user in searchedUsers" :key="user.id" @click="selectUser(user.email)">
             {{ user.email }}
           </li>
         </ul>
