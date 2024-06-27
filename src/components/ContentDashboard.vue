b978c8772ec17edc125f5c2f404f57ce8dd67528
EffahAdode3 <whatyousow00@gmail.com>
2024-06-27 11:01:26 +0000

    new push to the

--- src/components/ContentDashboard.vue
+++ src/components/ContentDashboard.vue
@@ -126,11 +126,11 @@
           <td>
           <div v-if="canEditOrDelete(toTolist)">
             <button class="btn btn-danger btn-sm" @click="deleteTodo(toTolist.id)">Delete</button>
-            <button class="btn btn-primary btn-sm" @click="editTodo(toTolist)">Edit</button>
+            <!-- <button class="btn btn-primary btn-sm" @click="editTodo(toTolist)">Edit</button> -->
           </div>
           <div v-else>
             <button class="btn btn-danger btn-sm" disabled>Delete</button>
-            <button class="btn btn-primary btn-sm" disabled>Edit</button>
+            <!-- <button class="btn btn-primary btn-sm" disabled>Edit</button> -->
             <!-- <small class="text-muted">Not authorized to edit or delete</small> -->
             <span class="text-muted">Not authorized</span>
           </div>
