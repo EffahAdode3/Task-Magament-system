ccbb96a3dddb8173c5a0c1cc1179f3f21fd00cb4
EffahAdode3 <whatyousow00@gmail.com>
2024-08-01 22:40:43 +0000

    upload file to database

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-// export const base_url="https://task-managment-system-backend-api.onrender.com"
- export const base_url="http://localhost:9878"
+export const base_url="https://task-managment-system-backend-api.onrender.com"
+//  export const base_url="http://localhost:9878"
 
